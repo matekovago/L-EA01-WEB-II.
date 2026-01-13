ba0da2f066c4e84b5d8be2adf4fe499b600c0d3d
Daniii108 <127598236+Daniii108@users.noreply.github.com>
2026-01-13 23:31:27 +0100

    add routes

--- routes/web.php
+++ routes/web.php
@@ -26,16 +26,18 @@ Route::get('/uzenetek', function () {
     return view('uzenetek');
 })->middleware('auth');
 
-use App\Http\Controllers\AuthController;
 
-Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
-Route::post('/register', [AuthController::class, 'register']);
-Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
-Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+use App\Http\Controllers\AuthController;
+    Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
 use App\Http\Controllers\KapcsolatController;
+    Route::get('/kapcsolat', [KapcsolatController::class, 'showForm'])->name('kapcsolat');
+    Route::post('/kapcsolat', [KapcsolatController::class, 'sendMessage'])->name('kapcsolat.send');
 
-Route::get('/kapcsolat', [KapcsolatController::class, 'showForm'])->name('kapcsolat');
-Route::post('/kapcsolat', [KapcsolatController::class, 'sendMessage'])->name('kapcsolat.send');
+use App\Http\Controllers\AdatbazisController;
+    Route::get('/adatbazis', [AdatbazisController::class, 'index'])->name('adatbazis');
 
